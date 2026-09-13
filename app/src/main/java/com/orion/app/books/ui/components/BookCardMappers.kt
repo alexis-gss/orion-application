alexis-gss/cinely-application

feat: :sparkles: add games and books domains

app/src/main/java/com/orion/app/books/ui/components/BookCardMappers.kt
@@ -1,0 +1,91 @@
+package com.orion.app.books.ui.components
+
+import android.content.Context
+import com.orion.app.R
+import com.orion.app.books.data.FavoriteBook
+import com.orion.app.books.data.FollowedBook
+import com.orion.app.books.data.HardcoverBook
+import com.orion.app.books.data.ReadBook
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+/** Dates are always rendered in French (see [com.orion.app.core.util.DateUtils]'s class doc), independent of the app's English UI. */
+private val frenchLocale = Locale.FRENCH
+
+fun HardcoverBook.toCardData(subtitle: String? = authorsLabel ?: year): BookCardData = BookCardData(
+    key = "hardcover_$id",
+    volumeId = id,
+    title = name,
+    coverUrl = coverUrl,
+    subtitle = subtitle,
+    rating = displayRating,
+    genres = primaryCategory,
+    releaseDate = publishedTimestamp?.let { it * 1000 }
+)
+
+fun FollowedBook.toCardData(context: Context): BookCardData = BookCardData(
+    key = "followed_$volumeId",
+    volumeId = volumeId,
+    title = title,
+    coverUrl = coverUrl,
+    subtitle = releaseLabel ?: releaseTimestamp?.toReadableDate(),
+    trailingText = if (isReleased) null else context.getString(R.string.book_status_upcoming),
+    // Same convention as ReadBook.toCardData: the generic segment (before " / ") of each
+    // CSV category, deduplicated — without this, BooksLibraryScreen's genre picker stayed
+    // empty even though followed books did have known categories.
+    genres = categories?.split(",")
+        ?.map { it.trim().substringBefore(" / ") }
+        ?.filter { it.isNotBlank() }
+        ?.distinct()
+        ?.joinToString(","),
+    releaseDate = releaseTimestamp?.let { it * 1000 }
+)
+
+fun ReadBook.toCardData(context: Context): BookCardData = BookCardData(
+    key = "read_$id",
+    volumeId = volumeId,
+    title = title,
+    coverUrl = coverUrl,
+    subtitle = statusLabel(context, status),
+    // Date the book was marked "read" (last status change), more useful here than the
+    // page count, which adds nothing once the book has already been read.
+    trailingText = updatedAt.toReadableDateFromMillis(),
+    // Same convention as BooksStatsScreen.BooksCategoryBreakdownCard: the generic segment
+    // (before " / ") of each CSV category, deduplicated.
+    genres = categories?.split(",")
+        ?.map { it.trim().substringBefore(" / ") }
+        ?.filter { it.isNotBlank() }
+        ?.distinct()
+        ?.joinToString(","),
+    addedAt = updatedAt
+)
+
+fun FavoriteBook.toCardData(dateLabel: String? = null): BookCardData = BookCardData(
+    key = "favorite_$volumeId",
+    volumeId = volumeId,
+    title = title,
+    coverUrl = coverUrl,
+    trailingText = dateLabel,
+    genres = categories?.split(",")
+        ?.map { it.trim().substringBefore(" / ") }
+        ?.filter { it.isNotBlank() }
+        ?.distinct()
+        ?.joinToString(","),
+    addedAt = addedAt
+)
+
+private fun statusLabel(context: Context, status: String): String = when (status) {
+    "reading" -> context.getString(R.string.book_status_reading)
+    "read" -> context.getString(R.string.book_read_label)
+    "dropped" -> context.getString(R.string.game_status_dropped)
+    "to_read" -> context.getString(R.string.book_status_to_read)
+    else -> status
+}
+
+private fun Long.toReadableDate(): String =
+    SimpleDateFormat("d MMM yyyy", frenchLocale).format(Date(this * 1000))
+
+/** Same as [toReadableDate], but for a timestamp already in milliseconds (e.g. ReadBook.updatedAt), unlike releaseTimestamp/publishedTimestamp which are in seconds. */
+private fun Long.toReadableDateFromMillis(): String =
+    SimpleDateFormat("d MMM yyyy", frenchLocale).format(Date(this))

app/src/main/java/com/orion/app/core/util/DateUtils.kt
@@ -1,0 +1,142 @@
+package com.orion.app.core.util
+
+import android.content.Context
+import com.orion.app.R
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.time.temporal.ChronoUnit
+import java.util.Date
+import java.util.Locale
+
+/**
+ * TMDB only provides a release DATE (no precise time — networks/platforms don't communicate
+ * one reliably or consistently). We therefore display the day in French, without inventing
+ * a time of day.
+ *
+ * IMPORTANT — language: day and month names produced here are intentionally always in French
+ * (via [frenchLocale]), independent of the app's UI language (English) or the device's system
+ * locale. Only the surrounding labels (e.g. "Today", "Tomorrow") come from English string
+ * resources. This matches the product decision to keep all displayed dates in French while
+ * the rest of the app's text is in English.
+ */
+object DateUtils {
+    private val isoFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+    private val frenchZone: ZoneId = ZoneId.of("Europe/Paris")
+    private val frenchLocale: Locale = Locale.FRENCH
+
+    /**
+     * Short "d MMM yyyy" format (e.g. "14 août 2026") used for date pills (added on / watched
+     * on / finished on...) on account pages. Shared by all three domains (cinema, games, books)
+     * to keep this display consistent everywhere in the app. Always formatted in French,
+     * regardless of the app's (English) UI language — see the class-level note above.
+     */
+    fun formatShortDate(timestampMillis: Long): String =
+        SimpleDateFormat("d MMM yyyy", frenchLocale).format(Date(timestampMillis))
+
+    /**
+     * Formats an ISO date (yyyy-MM-dd) as a full French day label, e.g. "Vendredi 14 août 2026".
+     * "Today" and "Tomorrow" come from localized (English) string resources; the day-of-week and
+     * month names are always rendered in French (see the class-level note above), independent
+     * of the app's UI locale.
+     */
+    fun formatFrenchDay(context: Context, isoDate: String?): String {
+        if (isoDate.isNullOrBlank()) return context.getString(R.string.date_unknown)
+        return try {
+            val date = LocalDate.parse(isoDate, isoFormatter)
+            val today = LocalDate.now(frenchZone)
+            when {
+                date.isEqual(today) -> context.getString(R.string.date_today)
+                date.isEqual(today.plusDays(1)) -> context.getString(R.string.date_tomorrow)
+                else -> {
+                    val dayName = date.dayOfWeek.getDisplayName(TextStyle.FULL, frenchLocale)
+                        .replaceFirstChar { it.uppercase(frenchLocale) }
+                    val monthName = date.month.getDisplayName(TextStyle.FULL, frenchLocale)
+                    if (date.year == today.year) {
+                        context.getString(R.string.date_format_same_year, dayName, date.dayOfMonth, monthName)
+                    } else {
+                        context.getString(R.string.date_format_other_year, dayName, date.dayOfMonth, monthName, date.year)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            isoDate
+        }
+    }
+
+    /**
+     * Short label for the number of days remaining before release, shown on the right of each
+     * Planning row (the full day is shown only once, in the group header, by PlanningScreen).
+     */
+    fun daysRemainingLabel(context: Context, isoDate: String?): String {
+        if (isoDate.isNullOrBlank()) return context.getString(R.string.days_remaining_unknown)
+        return try {
+            val date = LocalDate.parse(isoDate, isoFormatter)
+            val today = LocalDate.now(frenchZone)
+            val days = ChronoUnit.DAYS.between(today, date)
+            when {
+                days < 0 -> context.getString(R.string.days_remaining_released)
+                days == 0L -> context.getString(R.string.days_remaining_today)
+                days == 1L -> context.getString(R.string.days_remaining_tomorrow)
+                else -> context.getString(R.string.days_remaining_days, days)
+            }
+        } catch (e: Exception) {
+            context.getString(R.string.days_remaining_unknown)
+        }
+    }
+
+    /**
+     * Check if element is already released.
+     */
+    fun isReleased(isoDate: String?): Boolean {
+        if (isoDate.isNullOrBlank()) return false
+        return try {
+            LocalDate.parse(isoDate, isoFormatter).isBefore(LocalDate.now(frenchZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    /**
+     * Check if an episode can be marked as watched: already aired or airing today.
+     * Unlike isReleased (strictly before today), this includes the air date itself.
+     */
+    fun isWatchable(isoDate: String?): Boolean {
+        if (isoDate.isNullOrBlank()) return false
+        return try {
+            !LocalDate.parse(isoDate, isoFormatter).isAfter(LocalDate.now(frenchZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    /**
+     * True if the ISO date (yyyy-MM-dd, TMDB format) falls exactly today (Europe/Paris time
+     * zone). Used by the release-notification worker to spot followed movies/episodes
+     * releasing on the current day.
+     */
+    fun isIsoDateToday(isoDate: String?): Boolean {
+        if (isoDate.isNullOrBlank()) return false
+        return try {
+            LocalDate.parse(isoDate, isoFormatter).isEqual(LocalDate.now(frenchZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    /**
+     * Equivalent of [isIsoDateToday] for an epoch-seconds timestamp (games/books, which store
+     * their release date this way rather than as an ISO string).
+     */
+    fun isEpochSecondsToday(epochSeconds: Long?): Boolean {
+        if (epochSeconds == null) return false
+        return try {
+            val date = java.time.Instant.ofEpochSecond(epochSeconds).atZone(frenchZone).toLocalDate()
+            date.isEqual(LocalDate.now(frenchZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+}

app/src/test/java/com/orion/app/core/util/DateUtilsTest.kt
@@ -1,0 +1,139 @@
+package com.orion.app.core.util
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.RobolectricTestRunner
+import java.time.LocalDate
+
+/**
+ * Unit tests for [DateUtils]. English UI labels ("Unknown date", "Released"...) come from
+ * `strings.xml`, while day/month names are always expected in French regardless of locale,
+ * per the app's date-formatting policy (see [DateUtils]'s class doc).
+ */
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34], shadows = [])
+class DateUtilsTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setup() {
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun `formatFrenchDay returns unknown date when input is null or blank`() {
+        assertEquals(
+            "Unknown date",
+            DateUtils.formatFrenchDay(context, null)
+        )
+        assertEquals(
+            "Unknown date",
+            DateUtils.formatFrenchDay(context, "")
+        )
+    }
+
+    @Test
+    fun `formatFrenchDay returns the raw input when the ISO format is invalid`() {
+        assertEquals(
+            "not a date",
+            DateUtils.formatFrenchDay(context, "not a date")
+        )
+    }
+
+    @Test
+    fun `formatFrenchDay returns a far past date without a day offset, in French`() {
+        val result = DateUtils.formatFrenchDay(context, "2020-01-03")
+
+        assertEquals(
+            "Vendredi 3 janvier 2020",
+            result
+        )
+    }
+
+    @Test
+    fun `formatFrenchDay returns a far future date without a day offset, in French`() {
+        val result = DateUtils.formatFrenchDay(context, "2030-08-07")
+
+        assertEquals(
+            "Mercredi 7 août 2030",
+            result
+        )
+    }
+
+    @Test
+    fun `formatFrenchDay omits the year when the date falls in the current year`() {
+        val currentYear = LocalDate.now().year
+
+        val result = DateUtils.formatFrenchDay(
+            context,
+            "$currentYear-12-25"
+        )
+
+        assertTrue(
+            "expected no year for the current year, got: $result",
+            !result.contains(currentYear.toString())
+        )
+    }
+
+    @Test
+    fun `daysRemainingLabel returns D-minus-n days for a far future date`() {
+        val farFuture = LocalDate.now().plusDays(10).toString()
+
+        assertEquals(
+            "D-10",
+            DateUtils.daysRemainingLabel(context, farFuture)
+        )
+    }
+
+    @Test
+    fun `daysRemainingLabel returns Released for a past date`() {
+        val past = LocalDate.now().minusDays(5).toString()
+
+        assertEquals(
+            "Released",
+            DateUtils.daysRemainingLabel(context, past)
+        )
+    }
+
+    @Test
+    fun `daysRemainingLabel returns a dash when the input is invalid`() {
+        assertEquals(
+            "—",
+            DateUtils.daysRemainingLabel(context, null)
+        )
+        assertEquals(
+            "—",
+            DateUtils.daysRemainingLabel(context, "invalid")
+        )
+    }
+
+    @Test
+    fun `isReleased is true only strictly before today`() {
+        val yesterday = LocalDate.now().minusDays(1).toString()
+        val today = LocalDate.now().toString()
+        val tomorrow = LocalDate.now().plusDays(1).toString()
+
+        assertTrue(DateUtils.isReleased(yesterday))
+        assertFalse(DateUtils.isReleased(today))
+        assertFalse(DateUtils.isReleased(tomorrow))
+    }
+
+    @Test
+    fun `isWatchable is true on and before today, false after`() {
+        val yesterday = LocalDate.now().minusDays(1).toString()
+        val today = LocalDate.now().toString()
+        val tomorrow = LocalDate.now().plusDays(1).toString()
+
+        assertTrue(DateUtils.isWatchable(yesterday))
+        assertTrue(DateUtils.isWatchable(today))
+        assertFalse(DateUtils.isWatchable(tomorrow))
+    }
+}

alexis-gss/orion-application

app/src/main/java/com/orion/app/books/ui/components/BookCardMappers.kt
@@ -1,0 +1,91 @@
+package com.orion.app.books.ui.components
+
+import android.content.Context
+import com.orion.app.R
+import com.orion.app.books.data.FavoriteBook
+import com.orion.app.books.data.FollowedBook
+import com.orion.app.books.data.HardcoverBook
+import com.orion.app.books.data.ReadBook
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+/** Dates are always rendered in French (see [com.orion.app.core.util.DateUtils]'s class doc), independent of the app's English UI. */
+private val targetLocale = Locale.US
+
+fun HardcoverBook.toCardData(subtitle: String? = authorsLabel ?: year): BookCardData = BookCardData(
+    key = "hardcover_$id",
+    volumeId = id,
+    title = name,
+    coverUrl = coverUrl,
+    subtitle = subtitle,
+    rating = displayRating,
+    genres = primaryCategory,
+    releaseDate = publishedTimestamp?.let { it * 1000 }
+)
+
+fun FollowedBook.toCardData(context: Context): BookCardData = BookCardData(
+    key = "followed_$volumeId",
+    volumeId = volumeId,
+    title = title,
+    coverUrl = coverUrl,
+    subtitle = releaseLabel ?: releaseTimestamp?.toReadableDate(),
+    trailingText = if (isReleased) null else context.getString(R.string.book_status_upcoming),
+    // Same convention as ReadBook.toCardData: the generic segment (before " / ") of each
+    // CSV category, deduplicated — without this, BooksLibraryScreen's genre picker stayed
+    // empty even though followed books did have known categories.
+    genres = categories?.split(",")
+        ?.map { it.trim().substringBefore(" / ") }
+        ?.filter { it.isNotBlank() }
+        ?.distinct()
+        ?.joinToString(","),
+    releaseDate = releaseTimestamp?.let { it * 1000 }
+)
+
+fun ReadBook.toCardData(context: Context): BookCardData = BookCardData(
+    key = "read_$id",
+    volumeId = volumeId,
+    title = title,
+    coverUrl = coverUrl,
+    subtitle = statusLabel(context, status),
+    // Date the book was marked "read" (last status change), more useful here than the
+    // page count, which adds nothing once the book has already been read.
+    trailingText = updatedAt.toReadableDateFromMillis(),
+    // Same convention as BooksStatsScreen.BooksCategoryBreakdownCard: the generic segment
+    // (before " / ") of each CSV category, deduplicated.
+    genres = categories?.split(",")
+        ?.map { it.trim().substringBefore(" / ") }
+        ?.filter { it.isNotBlank() }
+        ?.distinct()
+        ?.joinToString(","),
+    addedAt = updatedAt
+)
+
+fun FavoriteBook.toCardData(dateLabel: String? = null): BookCardData = BookCardData(
+    key = "favorite_$volumeId",
+    volumeId = volumeId,
+    title = title,
+    coverUrl = coverUrl,
+    trailingText = dateLabel,
+    genres = categories?.split(",")
+        ?.map { it.trim().substringBefore(" / ") }
+        ?.filter { it.isNotBlank() }
+        ?.distinct()
+        ?.joinToString(","),
+    addedAt = addedAt
+)
+
+private fun statusLabel(context: Context, status: String): String = when (status) {
+    "reading" -> context.getString(R.string.book_status_reading)
+    "read" -> context.getString(R.string.book_read_label)
+    "dropped" -> context.getString(R.string.game_status_dropped)
+    "to_read" -> context.getString(R.string.book_status_to_read)
+    else -> status
+}
+
+private fun Long.toReadableDate(): String =
+    SimpleDateFormat("d MMM yyyy", targetLocale).format(Date(this * 1000))
+
+/** Same as [toReadableDate], but for a timestamp already in milliseconds (e.g. ReadBook.updatedAt), unlike releaseTimestamp/publishedTimestamp which are in seconds. */
+private fun Long.toReadableDateFromMillis(): String =
+    SimpleDateFormat("d MMM yyyy", targetLocale).format(Date(this))

app/src/main/java/com/orion/app/core/util/DateUtils.kt
@@ -1,0 +1,139 @@
+package com.orion.app.core.util
+
+import android.content.Context
+import com.orion.app.R
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.time.temporal.ChronoUnit
+import java.util.Date
+import java.util.Locale
+
+/**
+ * TMDB only provides a release DATE (no precise time — networks/platforms don't communicate
+ * one reliably or consistently).
+ *
+ * IMPORTANT — language: day and month names produced here are intentionally always in English
+ * (via [targetLocale]), independent of the app's UI language (English) or the device's system
+ * locale. Only the surrounding labels (e.g. "Today", "Tomorrow") come from English string
+ * resources.
+ */
+object DateUtils {
+    private val isoFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+    private val targetZone: ZoneId = ZoneId.of("Europe/Paris")
+    private val targetLocale: Locale = Locale.US
+
+    /**
+     * Short "d MMM yyyy" format (e.g. "14 août 2026") used for date pills (added on / watched
+     * on / finished on...) on account pages. Shared by all three domains (cinema, games, books)
+     * to keep this display consistent everywhere in the app.
+     */
+    fun formatShortDate(timestampMillis: Long): String =
+        SimpleDateFormat("d MMM yyyy", targetLocale).format(Date(timestampMillis))
+
+    /**
+     * Formats an ISO date (yyyy-MM-dd) as a full English day label, e.g. "Vendredi 14 août 2026".
+     * "Today" and "Tomorrow" come from localized (English) string resources; the day-of-week and
+     * month names are always rendered in English (see the class-level note above), independent
+     * of the app's UI locale.
+     */
+    fun formatDay(context: Context, isoDate: String?): String {
+        if (isoDate.isNullOrBlank()) return context.getString(R.string.date_unknown)
+        return try {
+            val date = LocalDate.parse(isoDate, isoFormatter)
+            val today = LocalDate.now(targetZone)
+            when {
+                date.isEqual(today) -> context.getString(R.string.date_today)
+                date.isEqual(today.plusDays(1)) -> context.getString(R.string.date_tomorrow)
+                else -> {
+                    val dayName = date.dayOfWeek.getDisplayName(TextStyle.FULL, targetLocale)
+                        .replaceFirstChar { it.uppercase(targetLocale) }
+                    val monthName = date.month.getDisplayName(TextStyle.FULL, targetLocale)
+                    if (date.year == today.year) {
+                        context.getString(R.string.date_format_same_year, dayName, date.dayOfMonth, monthName)
+                    } else {
+                        context.getString(R.string.date_format_other_year, dayName, date.dayOfMonth, monthName, date.year)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            isoDate
+        }
+    }
+
+    /**
+     * Short label for the number of days remaining before release, shown on the right of each
+     * Planning row (the full day is shown only once, in the group header, by PlanningScreen).
+     */
+    fun daysRemainingLabel(context: Context, isoDate: String?): String {
+        if (isoDate.isNullOrBlank()) return context.getString(R.string.days_remaining_unknown)
+        return try {
+            val date = LocalDate.parse(isoDate, isoFormatter)
+            val today = LocalDate.now(targetZone)
+            val days = ChronoUnit.DAYS.between(today, date)
+            when {
+                days < 0 -> context.getString(R.string.days_remaining_released)
+                days == 0L -> context.getString(R.string.days_remaining_today)
+                days == 1L -> context.getString(R.string.days_remaining_tomorrow)
+                else -> context.getString(R.string.days_remaining_days, days)
+            }
+        } catch (e: Exception) {
+            context.getString(R.string.days_remaining_unknown)
+        }
+    }
+
+    /**
+     * Check if element is already released.
+     */
+    fun isReleased(isoDate: String?): Boolean {
+        if (isoDate.isNullOrBlank()) return false
+        return try {
+            LocalDate.parse(isoDate, isoFormatter).isBefore(LocalDate.now(targetZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    /**
+     * Check if an episode can be marked as watched: already aired or airing today.
+     * Unlike isReleased (strictly before today), this includes the air date itself.
+     */
+    fun isWatchable(isoDate: String?): Boolean {
+        if (isoDate.isNullOrBlank()) return false
+        return try {
+            !LocalDate.parse(isoDate, isoFormatter).isAfter(LocalDate.now(targetZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    /**
+     * True if the ISO date (yyyy-MM-dd, TMDB format) falls exactly today (Europe/Paris time
+     * zone). Used by the release-notification worker to spot followed movies/episodes
+     * releasing on the current day.
+     */
+    fun isIsoDateToday(isoDate: String?): Boolean {
+        if (isoDate.isNullOrBlank()) return false
+        return try {
+            LocalDate.parse(isoDate, isoFormatter).isEqual(LocalDate.now(targetZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    /**
+     * Equivalent of [isIsoDateToday] for an epoch-seconds timestamp (games/books, which store
+     * their release date this way rather than as an ISO string).
+     */
+    fun isEpochSecondsToday(epochSeconds: Long?): Boolean {
+        if (epochSeconds == null) return false
+        return try {
+            val date = java.time.Instant.ofEpochSecond(epochSeconds).atZone(targetZone).toLocalDate()
+            date.isEqual(LocalDate.now(targetZone))
+        } catch (e: Exception) {
+            false
+        }
+    }
+}

app/src/test/java/com/orion/app/core/util/DateUtilsTest.kt
@@ -1,0 +1,139 @@
+package com.orion.app.core.util
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.RobolectricTestRunner
+import java.time.LocalDate
+
+/**
+ * Unit tests for [DateUtils]. English UI labels ("Unknown date", "Released"...) come from
+ * `strings.xml`, while day/month names are always expected in English regardless of locale,
+ * per the app's date-formatting policy (see [DateUtils]'s class doc).
+ */
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34], shadows = [])
+class DateUtilsTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setup() {
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun `formatDay returns unknown date when input is null or blank`() {
+        assertEquals(
+            "Unknown date",
+            DateUtils.formatDay(context, null)
+        )
+        assertEquals(
+            "Unknown date",
+            DateUtils.formatDay(context, "")
+        )
+    }
+
+    @Test
+    fun `formatDay returns the raw input when the ISO format is invalid`() {
+        assertEquals(
+            "not a date",
+            DateUtils.formatDay(context, "not a date")
+        )
+    }
+
+    @Test
+    fun `formatDay returns a far past date without a day offset`() {
+        val result = DateUtils.formatDay(context, "2020-01-03")
+
+        assertEquals(
+            "Friday January 3 2020",
+            result
+        )
+    }
+
+    @Test
+    fun `formatDay returns a far future date without a day offset`() {
+        val result = DateUtils.formatDay(context, "2030-08-07")
+
+        assertEquals(
+            "Wednesday August 7 2030",
+            result
+        )
+    }
+
+    @Test
+    fun `formatDay omits the year when the date falls in the current year`() {
+        val currentYear = LocalDate.now().year
+
+        val result = DateUtils.formatDay(
+            context,
+            "$currentYear-12-25"
+        )
+
+        assertTrue(
+            "expected no year for the current year, got: $result",
+            !result.contains(currentYear.toString())
+        )
+    }
+
+    @Test
+    fun `daysRemainingLabel returns D-minus-n days for a far future date`() {
+        val farFuture = LocalDate.now().plusDays(10).toString()
+
+        assertEquals(
+            "D-10",
+            DateUtils.daysRemainingLabel(context, farFuture)
+        )
+    }
+
+    @Test
+    fun `daysRemainingLabel returns Released for a past date`() {
+        val past = LocalDate.now().minusDays(5).toString()
+
+        assertEquals(
+            "Released",
+            DateUtils.daysRemainingLabel(context, past)
+        )
+    }
+
+    @Test
+    fun `daysRemainingLabel returns a dash when the input is invalid`() {
+        assertEquals(
+            "—",
+            DateUtils.daysRemainingLabel(context, null)
+        )
+        assertEquals(
+            "—",
+            DateUtils.daysRemainingLabel(context, "invalid")
+        )
+    }
+
+    @Test
+    fun `isReleased is true only strictly before today`() {
+        val yesterday = LocalDate.now().minusDays(1).toString()
+        val today = LocalDate.now().toString()
+        val tomorrow = LocalDate.now().plusDays(1).toString()
+
+        assertTrue(DateUtils.isReleased(yesterday))
+        assertFalse(DateUtils.isReleased(today))
+        assertFalse(DateUtils.isReleased(tomorrow))
+    }
+
+    @Test
+    fun `isWatchable is true on and before today, false after`() {
+        val yesterday = LocalDate.now().minusDays(1).toString()
+        val today = LocalDate.now().toString()
+        val tomorrow = LocalDate.now().plusDays(1).toString()
+
+        assertTrue(DateUtils.isWatchable(yesterday))
+        assertTrue(DateUtils.isWatchable(today))
+        assertFalse(DateUtils.isWatchable(tomorrow))
+    }
+}